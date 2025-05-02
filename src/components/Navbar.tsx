a74c0e3cab84847ce2afc3b47df6e893171b5eda
Angel Aquino <aa23-0272@unphu.edu.do>
2025-05-01 21:09:38 -0400

    navbar design without responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,47 +1,59 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom";
 
 const navbarLinks = [
-    {
-        id: 1,
-        title: 'File to PDF',
-        href: '/FiletoPDF',
-    },
-    {
-        id: 2,
-        title: 'URL to PDF',
-        href: '/URLtoPDF',
-    },
-    {
-        id: 3,
-        title: 'Image to PDF',
-        href: '/ImagetoPDF',
-    },
-    {
-        id: 4,
-        title: 'HTML to PDF',
-        href: '/HTMLtoPDF',
-    },
-]; 
+  {
+    id: 1,
+    title: 'File to PDF',
+    href: '/file-to-pdf',
+  },
+  {
+    id: 2,
+    title: 'URL to PDF',
+    href: '/url-to-pdf',
+  },
+  {
+    id: 3,
+    title: 'Image to PDF',
+    href: '/image-to-pdf',
+  },
+  {
+    id: 4,
+    title: 'HTML to PDF',
+    href: '/html-to-pdf',
+  },
+];
 
 export const Navbar = () => {
   return (
-    <nav className='space-x-5 hidden md:flex items-center justify-center'>
-        
+    <header className="w-full py-4 px-6 flex items-center justify-between">
+      <div className="flex-shrink-0">
+        <Link to="/">
+          <img
+            src="/convertImage.png"
+            alt="FileConverter logo"
+            className="h-12"
+          />
+        </Link>
+      </div>
+
+      <nav className="flex-1 flex justify-center space-x-6">
         {navbarLinks.map((link) => (
-          <NavLink 
-            key={link.id} 
-            to={link.href} 
-            onClick={() => console.log(link.title)}
-            className={({ isActive }) => 
-              isActive 
-                ? 'text-gray-300 hover:text-gray-600 transition mx-2' 
-                : 'text-white hover:text-gray-500 transition mx-2'
+          <NavLink
+            key={link.id}
+            to={link.href}
+            className={({ isActive }) =>
+              isActive
+                ? 'text-gray-500 font-semibold transition'
+                : 'text-white hover:text-gray-300 transition'
             }
           >
             {link.title}
           </NavLink>
         ))}
       </nav>
-      
+      {/* Botón de inicio de sesión alineado a la derecha */}
+
+      <div className="w-12" /> 
+    </header>
   );
 };
